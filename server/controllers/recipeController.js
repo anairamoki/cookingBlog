f127f769e949667ebe45375614cc3ba798c04b0e
Ariane Moki <ariane.moki@msn.com>
2021-11-10 15:57:03 +0000

    setting up mongoDB and creating category schema

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,3 +1,46 @@
+require('../models/database');
+const Category = require('../models/CategorySchema');
+
+
+
+// Get homepage
 exports.homepage = async(req, res) => {
   res.render('index', {title: 'My Celiac Husband - Home'});
 }
+
+//inserting data into cookingBlog DB
+async function insetCategoryData(){
+  try{
+    await Category.insertMany([
+      {
+        "name": "Thai",
+        "image": "thai-food.jpg"
+      },
+      {
+        "name": "American",
+        "image": "american-food.jpg"
+      }, 
+      {
+        "name": "Chinese",
+        "image": "chinese-food.jpg"
+      },
+      {
+        "name": "Mexican",
+        "image": "mexican-food.jpg"
+      }, 
+      {
+        "name": "Indian",
+        "image": "indian-food.jpg"
+      },
+      {
+        "name": "Spanish",
+        "image": "spanish-food.jpg"
+      }
+    ]);
+  } catch (error){
+    console.log('err', + error);
+  }
+}
+
+insetCategoryData()
+
