9d70713545423e451ae20644aa6e7a8bc31f4806
Ariane Moki <ariane.moki@msn.com>
2021-11-12 16:26:37 +0000

    Inserting data into cookingBlog DB from front page

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -5,42 +5,17 @@ const Category = require('../models/CategorySchema');
 
 // Get homepage
 exports.homepage = async(req, res) => {
-  res.render('index', {title: 'My Celiac Husband - Home'});
-}
-
-//inserting data into cookingBlog DB
-async function insetCategoryData(){
-  try{
-    await Category.insertMany([
-      {
-        "name": "Thai",
-        "image": "thai-food.jpg"
-      },
-      {
-        "name": "American",
-        "image": "american-food.jpg"
-      }, 
-      {
-        "name": "Chinese",
-        "image": "chinese-food.jpg"
-      },
-      {
-        "name": "Mexican",
-        "image": "mexican-food.jpg"
-      }, 
-      {
-        "name": "Indian",
-        "image": "indian-food.jpg"
-      },
-      {
-        "name": "Spanish",
-        "image": "spanish-food.jpg"
-      }
-    ]);
-  } catch (error){
-    console.log('err', + error);
+  //* inserting data into cookingBlog DB from front page
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render('index', {title: 'My Celiac Husband - Home', categories});
+  }catch (error) {
+    res.status(500).send({message: error.message || "Oops, something went wrong!"});
   }
 }
 
-insetCategoryData()
+
+
+
 
