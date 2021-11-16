00d931206d5c625351f9cb97de13ac871f26ec8c
Ariane Moki <ariane.moki@msn.com>
2021-11-16 16:55:17 +0000

    creating categories page

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -15,6 +15,19 @@ exports.homepage = async(req, res) => {
   }
 }
 
+// Get /categories
+// categories
+exports.exploreCategories = async(req, res) => {
+  //* inserting data into cookingBlog DB from front page
+  try {
+    const limitNumber = 20;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render('categories', {title: 'My Celiac Husband - View All Categories', categories});
+  }catch (error) {
+    res.status(500).send({message: error.message || "Oops, something went wrong!"});
+  }
+}
+
 
 
 
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -4,5 +4,6 @@ const recipeController = require('../controllers/recipeController');
 
 //* App routes 
 router.get('/', recipeController.homepage);
+router.get('/categories', recipeController.exploreCategories);
 
 module.exports = router;
